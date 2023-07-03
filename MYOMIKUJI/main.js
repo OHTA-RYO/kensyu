517cd12cb2a40f8f3eda0fb1e4a6558324c3ada6
OHTA-RYO <oota230601@gmail.com>
2023-07-03 20:56:43 +0900

    おみくじ作成

--- MYOMIKUJI/main.js
+++ MYOMIKUJI/main.js
@@ -4,19 +4,27 @@
   const btn = document.getElementById("btn");
   const result = document.getElementById("result");
   btn.addEventListener("click", () => {
-    const n = Math.floor(Math.random() * 3);
+    const results = ['大吉','中吉','凶','小吉'];
+    //↑追加
+    const n = Math.floor(Math.random() * results.length);
+                            //↑lengthに配列の数と文字数がある。
+    result.textContent = results[n];
+    //↑追加　　　　　　　　　　↑のnはどういう意味かが
+    //わかりません。n番目を取ってくるのはなぜエラーにならないのか。
+    //↓の書き方と↑の書き方が同じ理由がわからない。
     
-    switch (n) {
-      case 0:
-        result.textContent ='大吉';
-        break;
-        case 1:
-          result.textContent = '中吉';
-          break;
-          case 2:
-            result.textContent = '凶';
-            break;
-    }
+    // switch (n) {
+    //   case 0:
+    //     result.textContent ='大吉';
+    //     break;
+    //     case 1:
+    //       result.textContent = '中吉';
+    //       break;
+    //       case 2:
+    //         result.textContent = '凶';
+    //         break;
+    // }
+
     //result.textContent = n;
     //教材と違うパターンで表示させた。
     // if (n === 0) {
