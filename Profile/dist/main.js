0965be49a08cd60dee85f12445c1313499ff84dd
OHTA-RYO <oota230601@gmail.com>
2023-07-12 03:09:07 +0900

    Profile 子要素の追加を作成

--- Profile/dist/main.js
+++ Profile/dist/main.js
@@ -2,12 +2,26 @@
 {
     const name = document.querySelector('.name');
     const rightSaves = document.querySelectorAll('.right-save');
-    const n = rightSaves.length;
-    rightSaves.forEach(() => {
+    const save = document.getElementById('save');
+    const right = document.getElementById('right');
+    let n = 1;
+    save === null || save === void 0 ? void 0 : save.addEventListener('click', () => {
+        const pRight = document.createElement('p');
+        pRight.id = 'appendp' + n;
+        pRight.innerText = '名前';
+        n++;
+        right === null || right === void 0 ? void 0 : right.appendChild(pRight);
+    });
+    save === null || save === void 0 ? void 0 : save.addEventListener('click', () => {
+        const iRight = document.createElement('input');
+        iRight.id = 'appendi' + n;
+        iRight.innerText = '';
+        n++;
+        right === null || right === void 0 ? void 0 : right.appendChild(iRight);
+    });
+    rightSaves.forEach((open) => {
         name === null || name === void 0 ? void 0 : name.addEventListener('click', () => {
-            var _a;
-            (_a = rightSaves[0]) === null || _a === void 0 ? void 0 : _a.classList.toggle('right-save');
-            console.log('right-save');
+            open.classList.toggle('right-save');
         });
     });
 }
--- Profile/main.ts
+++ Profile/main.ts
@@ -2,17 +2,73 @@
 {
 const name:Element | null = document.querySelector('.name');
 const rightSaves = document.querySelectorAll('.right-save');
-const n = rightSaves.length;
+//保存を取得
+const save = document.getElementById('save');
+//↓保存先の親要素を取得
+const right = document.getElementById('right'); 
+let n = 1;
 
-rightSaves.forEach(() => {
+//左側の情報を↓にとってこないといけない。
+save?.addEventListener('click',() =>{
+  //↓子要素pを追加する
+  const pRight =document.createElement('p');
+  //↓IDを作成
+  pRight.id = 'appendp' + n;
+  //↓タイトル
+  pRight.innerText = '名前';
+  //↓1クリックずつ増やす
+  n++;
+  //↓親にpを追加
+  right?.appendChild(pRight);
+});
+save?.addEventListener('click',() =>{
+  //↓子要素inputを一括追加
+  const iRight =document.createElement('input');
+  iRight.id = 'appendi' + n;
+  iRight.innerText = '';
+  n++;
+  right?.appendChild(iRight);
+});
+
+
+
+
+
+//          　　　元々↓をからにして10行目をrightSaveにしてた。そのような書き方は違うのでしょうか？
+rightSaves.forEach((open) => {
 name?.addEventListener('click',() => {
-rightSaves[0].classList.toggle('right-save');
-//↑[0]指定しないとクラスリストが発動しないから
-//入れた。。
+open.classList.toggle('right-save');
+// ↑全部出てしまう。
+
+//↓やりたかった処理2個目をクリックしたら1個目が閉じる
+// rightSaves.forEach((close) =>{
+//   if(open !== close){
+//     close.classList.remove('right-save');
+//   }
+// })
 
 });
 });
-// name?.addEventListener('click',() =>{
-// rightSaves[n].classList.remove('right-save');
-// });
 }
+
+
+//一括で要素を取得する方法
+// const np = document.createDocumentFragment();
+// const ni = document.createDocumentFragment();
+// for(let n = 1; n <=5; n++){
+// const pRight =document.createElement('p');
+// pRight.id = 'appendp' + n;
+// pRight.innerText = '名前' + n;
+// np.appendChild(pRight);
+// }
+// right?.appendChild(np);
+// console.log(np);
+
+// for(let n = 1; n <=5; n++){
+//   const iRight =document.createElement('input');
+//   iRight.id = 'appendi' + n;
+//   iRight.innerText = '' + n;
+//   ni.appendChild(iRight);
+//   }
+//   right?.appendChild(ni);
+//   console.log(np);
