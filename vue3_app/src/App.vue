5e49d60ca0436f8e074e008d2dd53e11ff0485f4
OHTA-RYO <oota230601@gmail.com>
2023-08-02 00:26:00 +0900

    sample追加

--- vue3_app/src/App.vue
+++ vue3_app/src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import Main from "./components/Main.vue";
+// import sampleN from "./components/sampleN.vue";
 // import Test from "./components/Test.vue";
 </script>
 
 <template>
   <Main />
+  <!-- <sampleN /> -->
   <!-- <Test /> -->
 </template>
 
